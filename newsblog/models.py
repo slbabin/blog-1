318191efe9b7c308f1923b80696779b5a0c13c3e
slbabin <>
2023-09-26 20:08:34 +0000

    Add approve comments method

--- newsblog/models.py
+++ newsblog/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
     post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    body = models.TextField()
+    body = HTMLField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
