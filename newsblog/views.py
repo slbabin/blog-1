cc85b691bb1aaa2da11d2b724c643846b1fbce37
slbabin <>
2023-09-26 20:32:15 +0000

    Create a view for PostList

--- newsblog/views.py
+++ newsblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BlogPost
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_time')
+    template_name = 'index.html'
+    paginate_by = 6
+
